51347e2b737429ad90b0257fc8127f6cf4da0ea7
Achhu <88780913+Achhu@users.noreply.github.com>
2022-11-02 20:31:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function brother()
+{
+    window.location = "brother.html";
+}
+
+function mother()
+{
+    window.location = "mother.html";
+}
+
+function father()
+{
+window.location = "father.html";
+}
+
+function me()
+{
+    window.location = "ME.html"
+}
+
+function setup()
+{
+    canvas = createCanvas(300 , 300);
+    canvas.center()
+}
+
+function draw()
+{
+    
+}
